31a21366a44ca7a4c94681d858ecb1d3cbc831a1
Wolfgang Hoenig <hoenig@tu-berlin.de>
2024-11-24 22:16:14 +0100

    add template

--- run-pandoc.sh
+++ run-pandoc.sh
@@ -1,3 +1,3 @@
 file="$1"
 shift
-pandoc --citeproc --bibliography *.bib --css media/custom.css --mathjax --standalone -t revealjs -V revealjs-url:reveal.js $file.md -o $file.html $@
+pandoc --citeproc --template media/template.html --css media/custom.css --mathjax --standalone -t revealjs -V revealjs-url:reveal.js $file.md -o $file.html $@
